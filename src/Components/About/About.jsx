0e6df6a3ac0ba895b4c43ce5335f9d43e044665e
Olamayorrh <105288976+Olamayorrh@users.noreply.github.com>
2024-05-11 06:55:22 +0100

    update

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,23 @@
+import Button from "../Button/Button";
+import "./About.css"
+const About = ()=>{
+    return(
+        <div className="about">
+            <div className="inner">
+                 <h1>TAYLOR SWIFT</h1><h2>THE ERAS TOUR</h2><h3>TAYLOR's VERSION</h3>
+
+                <div>
+                    <p>Includes "cardigan" and four addtitional accustic songs</p>
+                    <p>Now streaming on Disney+.</p>
+                    <Button text="SUBSCRIBE NOW*" />
+                </div>
+            </div>
+            
+
+        </div>
+    )
+}
+
+export default About;
+
+
